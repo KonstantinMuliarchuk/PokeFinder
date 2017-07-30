bc0a27346f94d5a54bc34cec3d1de1d5586ea8a7
Konstantin Muliarchuk <kotipod@gmail.com>
2017-07-30 21:08:38 +0300

    first commit

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Мулярчук Константин on 30.07.17.
+//  Copyright © 2017 healthAndFitnes. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+
+#endif /* PokeFinder_Bridging_Header_h */
